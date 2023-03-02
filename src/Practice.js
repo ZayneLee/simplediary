a23f6d67cd2c050f249020a0a8b8ee1d36ceb9b6
Zayne <star1582@naver.com>
2023-03-02 22:13:39 +0900

    Object

--- src/Practice.js
+++ src/Practice.js
@@ -1,25 +1,39 @@
-// 콜백 함수 : 함수의 파라미터로 함수를 넘기는 것
+// 객체 리터럴 방식으로 객체 생성
+const person = {
+  name: "Zayne", // 객체 프로퍼티
+  gender: "male",
+};
 
-function chekcMood(mood, goodCallback, badCallback) {
-  if (mood === "good") {
-    // 기분 좋을 때
-    goodCallback();
-  } else {
-    // 기분 안좋을 때
-    badCallback();
-  }
+// 점 표기법으로 객체 프로퍼티에 접근
+console.log(person.name);
+
+// 괄호 표기법으로 객체 프로퍼티에 접근
+// 괄호 표기법으로 접근시에는 키 값을 문자 형태로 전달
+console.log(person["gender"]);
+
+function getPropertyValue(key) {
+  return person[key];
 }
 
-function cry() {
-  console.log("ACTION :: CRY");
-}
+// 동적인 파라미터를 받는 상황에서는 괄호 표기법을 통해 객체 프로퍼티에 접근할 수 있다.
+console.log(getPropertyValue("name")); // Zayne
 
-function sing() {
-  console.log("ACTION :: SING");
-}
+// 객체 프로퍼티 추가
+person.location = "Korea";
+person["age"] = 33;
+console.log(person); //{name: 'Zayne', gender: 'male', location: 'Korea', age: 33}
 
-function dance() {
-  console.log("ACTION :: DANCE");
-}
+// 객체 프로퍼티 수정
+person.location = "UK";
+person["age"] = 30;
+console.log(person); // {name: 'Zayne', gender: 'male', location: 'UK', age: 30}
 
-chekcMood("good", sing, cry);
+// person 객체가 const 로 선언되었음에도 프로퍼티 추가 및 수정이 가능
+
+// 객체 프로퍼티 삭제
+delete person.age;
+delete person["location"];
+console.log(person); //{name: 'Zayne', gender: 'male'}
+
+// 객체 프로퍼티 확인
+console.log(`name : ${"name" in person}`); // true
